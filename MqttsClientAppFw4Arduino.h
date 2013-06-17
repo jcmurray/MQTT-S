73191bdd102ba258518d77c0304dddcb8f539a8a
Tomoaki YAMAGUCHI <tomoaki@tomy-tech.com>
2013-06-18 05:40:00 +0900

    Update MqttsClientAppFw4Arduino.h

--- MqttsClientAppFw4Arduino.h
+++ MqttsClientAppFw4Arduino.h
@@ -1,6 +1,8 @@
 /*
  * MqttsClientApplication.h
-*               Copyright (c) 2013 Tomoaki YAMAGUCHI  All rights reserved.
+ *
+ *                          The MIT License (MIT)
+ *               Copyright (c) 2013 Tomoaki YAMAGUCHI  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -22,8 +24,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * You should have received a copy of the GNU General Public License
- * If not, see <http://www.gnu.org/licenses/>.
  *
  *
  *  Created on: 2013/06/15
